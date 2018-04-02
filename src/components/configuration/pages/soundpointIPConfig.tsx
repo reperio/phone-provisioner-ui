f1d34b3bd7a73c96350c38e7d5f4415dd2c07828
ZakFahey <z.fahey@gmail.com>
2018-04-02 12:53:16 -0400

    Added message waiting properties

--- src/components/configuration/pages/soundpointIPConfig.tsx
+++ src/components/configuration/pages/soundpointIPConfig.tsx
@@ -21,6 +21,7 @@ const possibleCodecPrefValues: string[] = [
     'Siren14.48kbps',
     'Siren22.64kbps'
 ];
+const possibleCallBackModeValues = ['contact', 'registration', 'disabled'];
 
 interface IComponentProps {
     actions?: any;
@@ -31,47 +32,65 @@ class SoundpointIPConfig extends React.Component<IComponentProps, {}> {
     props: IComponentProps;
 
     render() {
+
         return (
             <PassPropsToChildren options={this.props.options}>
                 <PolycomConfig/>
 
-                <TextPropertyContainer propertyName={'digitMap'}>Digit Map</TextPropertyContainer>
-                <BooleanPropertyContainer propertyName={'tagSerialNo'}>Tag Serial Number</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'oneTouchVoiceMail'}>One Touch Voicemail</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'ztpEnabled'}>ZTP Enabled</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'presence'}>Presence</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'messaging'}>Messaging</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'callWaiting'}>Call Waiting</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'urlModeDialing'}>URL Mode Dialing</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'urlDialing'}>URL Dialing</BooleanPropertyContainer>
+                <TextPropertyContainer propertyName='digitMap'>Digit Map</TextPropertyContainer>
+                <BooleanPropertyContainer propertyName='tagSerialNo'>Tag Serial Number</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='oneTouchVoiceMail'>One Touch Voicemail</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='ztpEnabled'>ZTP Enabled</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='presence'>Presence</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='messaging'>Messaging</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='callWaiting'>Call Waiting</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='urlModeDialing'>URL Mode Dialing</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='urlDialing'>URL Dialing</BooleanPropertyContainer>
 
-                <SortableListPropertyContainer propertyName={'codecPref'} possibleValues={possibleCodecPrefValues}>Codec Preference</SortableListPropertyContainer>
+                <SortableListPropertyContainer propertyName='codecPref' possibleValues={possibleCodecPrefValues}>Codec Preference</SortableListPropertyContainer>
 
                 <h3>Message Waiting</h3>
-                <BooleanPropertyContainer propertyName={'bypassInstantMessage'}>Bypass Instant Message</BooleanPropertyContainer>
-                {/*TODO: MWI properties*/}
+                <BooleanPropertyContainer propertyName='bypassInstantMessage'>Bypass Instant Message</BooleanPropertyContainer>
+                <h5>MWI-1</h5>
+                <DropdownPropertyContainer propertyName='mwi1_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi1_callBack'>Callback</TextPropertyContainer>
+                <h5>MWI-2</h5>
+                <DropdownPropertyContainer propertyName='mwi2_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi2_callBack'>Callback</TextPropertyContainer>
+                <h5>MWI-3</h5>
+                <DropdownPropertyContainer propertyName='mwi3_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi3_callBack'>Callback</TextPropertyContainer>
+                <h5>MWI-4</h5>
+                <DropdownPropertyContainer propertyName='mwi4_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi4_callBack'>Callback</TextPropertyContainer>
+                <h5>MWI-5</h5>
+                <DropdownPropertyContainer propertyName='mwi5_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi6_callBack'>Callback</TextPropertyContainer>
+                <h5>MWI-6</h5>
+                <DropdownPropertyContainer propertyName='mwi6_callBackMode' possibleValues={possibleCallBackModeValues}>Callback Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='mwi6_callBack'>Callback</TextPropertyContainer>
 
                 <h3>Provisioning Polling</h3>
-                <BooleanPropertyContainer propertyName={'pollingEnabled'}>Enabled</BooleanPropertyContainer>
-                <DropdownPropertyContainer propertyName={'pollingMode'} possibleValues={['abs', 'rel', 'random']}>Mode</DropdownPropertyContainer>
-                <TextPropertyContainer propertyName={'pollingPeriod'} isInteger min={1}>Period</TextPropertyContainer>
-                <TimeSpanPropertyContainer propertyName={'pollingTime'}>Time</TimeSpanPropertyContainer>
-                <TimeSpanPropertyContainer propertyName={'pollingTimeRandomEnd'}>Random End</TimeSpanPropertyContainer>
+                <BooleanPropertyContainer propertyName='pollingEnabled'>Enabled</BooleanPropertyContainer>
+                <DropdownPropertyContainer propertyName='pollingMode' possibleValues={['abs', 'rel', 'random']}>Mode</DropdownPropertyContainer>
+                <TextPropertyContainer propertyName='pollingPeriod' isInteger min={1}>Period</TextPropertyContainer>
+                <TimeSpanPropertyContainer propertyName='pollingTime'>Time</TimeSpanPropertyContainer>
+                <TimeSpanPropertyContainer propertyName='pollingTimeRandomEnd'>Random End</TimeSpanPropertyContainer>
 
                 <h3>SNTP</h3>
-                <TextPropertyContainer propertyName={'sntpAddress'}>Address</TextPropertyContainer>
-                <TextPropertyContainer propertyName={'sntpGmtOffset'} isInteger>GMT Offset</TextPropertyContainer>
-                <TextPropertyContainer propertyName={'sntpResyncPeriod'} isInteger min={1}>Resync Period</TextPropertyContainer>
+                <TextPropertyContainer propertyName='sntpAddress'>Address</TextPropertyContainer>
+                <TextPropertyContainer propertyName='sntpGmtOffset' isInteger>GMT Offset</TextPropertyContainer>
+                <TextPropertyContainer propertyName='sntpResyncPeriod' isInteger min={1}>Resync Period</TextPropertyContainer>
 
                 <h3>Voice Activity Detection</h3>
-                <BooleanPropertyContainer propertyName={'vadEnable'}>Enable</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'vadSignalAnnexB'}>Signal Annex B</BooleanPropertyContainer>
-                <TextPropertyContainer propertyName={'vadThresh'} isInteger min={0} max={30}>Threshold</TextPropertyContainer>
+                <BooleanPropertyContainer propertyName='vadEnable'>Enable</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='vadSignalAnnexB'>Signal Annex B</BooleanPropertyContainer>
+                <TextPropertyContainer propertyName='vadThresh' isInteger min={0} max={30}>Threshold</TextPropertyContainer>
 
                 <h3>Volume Persist</h3>
-                <BooleanPropertyContainer propertyName={'volumePersistHandset'}>Handset</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'volumePersistHeadset'}>Headset</BooleanPropertyContainer>
-                <BooleanPropertyContainer propertyName={'volumePersistHandsFree'}>Hands Free</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='volumePersistHandset'>Handset</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='volumePersistHeadset'>Headset</BooleanPropertyContainer>
+                <BooleanPropertyContainer propertyName='volumePersistHandsFree'>Hands Free</BooleanPropertyContainer>
             </PassPropsToChildren>
         );
     }

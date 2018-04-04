cabe01cc68a629b56bdc90f50fc3e71e259ca36f
ZakFahey <z.fahey@gmail.com>
2018-04-04 11:11:14 -0400

    Fixed option height format issues

--- src/components/configuration/properties/configProperty.tsx
+++ src/components/configuration/properties/configProperty.tsx
@@ -22,7 +22,7 @@ class ConfigProperty extends React.Component<IComponentProps, {}> {
 
     render() {
         return (
-            <div className={'row'} style={{height: '100px'}}>
+            <div className={'row'} style={{minHeight: '80px'}}>
                 <div className={'col-sm-2'}>
                     <Switch onChange={this.togglePropertyInheritance} checked={!this.props.options.inherited}/>
                 </div>

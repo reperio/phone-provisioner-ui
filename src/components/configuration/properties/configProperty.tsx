d8d7e13b4d9f3e0100196d020aaa1b00583d2ad2
ZakFahey <z.fahey@gmail.com>
2018-04-10 15:04:15 -0400

    Fixed column alignment

--- src/components/configuration/properties/configProperty.tsx
+++ src/components/configuration/properties/configProperty.tsx
@@ -24,19 +24,19 @@ class ConfigProperty extends React.Component<IComponentProps, {}> {
     render() {
         return (
             <div className={'row'}>
-                <div className={'col-sm-2'}>
+                <div className={'col-sm-2 centered-column'}>
                     <Switch
                         onChange={this.togglePropertyInheritance}
                         checked={!this.props.options.inherited}
                         classes={{checked: 'selected-toggle', bar: !this.props.options.inherited ? 'selected-toggle-bar' : null}}
                     />
                 </div>
-                <div className={'col-sm-6'}>
+                <div className={'col-sm-6 centered-column'}>
                     {this.props.displayName && <div className='input-name'>{this.props.displayName}</div>}
                     {this.props.children}
 
                 </div>
-                <div className={'col-sm-4'}>
+                <div className={'col-sm-4 centered-column'}>
                     {this.props.options.inherited && `Inherited from ${ConfigLevelName(this.props.options.inheritLevel)}`}
                 </div>
             </div>

212a35c4c7a8ab4e55cb71abccef6286b792ee10
ZakFahey <z.fahey@gmail.com>
2018-05-15 15:17:44 -0400

    Fixed unit tests

--- test/reduxStores.ts
+++ test/reduxStores.ts
@@ -57,6 +57,41 @@ export const initialStateWithManufacturerLoaded: Store = {
     }
 }
 
+export const initialStateWithManufacturerAndOrganizationsLoaded: Store = {
+    configurationSettings: {
+        anyUnsavedChanges: false,
+        currentlyEditing: null,
+        allConfigs: [
+            {
+                component_name: "polycomConfig",
+                config: '{"test": true}',
+                default_config: '{"test": false, "test2": "Inherited!"}',
+                id: "fb6c87ee-5968-45f4-bf3e-0d82d812fec7",
+                name: "Polycom",
+                expanded: false,
+                children: null,
+            }
+        ],
+        currentOrganization: {
+            name: 'Test',
+            id: '1',
+            type: OrganizationType.NORMAL
+        },
+        organizations: [
+            {
+                name: 'Test',
+                id: '1',
+                type: OrganizationType.NORMAL
+            },
+            {
+                name: 'Test2',
+                id: '2',
+                type: OrganizationType.NORMAL
+            }
+        ]
+    }
+}
+
 export const initialStateWithFamilyLoaded: Store = {
     configurationSettings: {
         anyUnsavedChanges: false,
@@ -214,6 +249,73 @@ export const initialStateWithModelLoaded: Store = {
         ],
         currentOrganization: null,
         organizations: []
+    }
+}
+
+export const initialStateWithModelAndOrganizationsLoaded: Store = {
+    configurationSettings: {
+        anyUnsavedChanges: false,
+        currentlyEditing: null,
+        allConfigs: [
+            {
+                component_name: "polycomConfig",
+                config: '{"test": true}',
+                default_config: '{"test": false, "test2": "Inherited!"}',
+                id: "fb6c87ee-5968-45f4-bf3e-0d82d812fec7",
+                name: "Polycom",
+                expanded: true,
+                children: [
+                    {
+                        component_name: "soundpointIPConfig",
+                        config: '{"test2":"ayy"}',
+                        default_config: '{"something":""}',
+                        id: "188a8ddd-9a57-4f45-aac2-effd96933039",
+                        manufacturer: "fb6c87ee-5968-45f4-bf3e-0d82d812fec7",
+                        name: "Soundpoint IP",
+                        expanded: true,
+                        children: [
+                            {
+                                component_name: "soundpointIP330Config",
+                                config: '{}',
+                                default_config: '{"extra": "hey"}',
+                                id: "646e4a66-823c-48fc-80e1-547cb5f67532",
+                                family: "188a8ddd-9a57-4f45-aac2-effd96933039",
+                                name: "330",
+                                expanded: false,
+                                children: null
+                            },
+                            {
+                                component_name: "soundpointIP335Config",
+                                config: '{}',
+                                default_config: '{"extra2": true}',
+                                id: "1ceebd84-b735-4a90-ac51-854c7ac01b2c",
+                                family: "188a8ddd-9a57-4f45-aac2-effd96933039",
+                                name: "335",
+                                expanded: false,
+                                children: null
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        currentOrganization: {
+            name: 'Test',
+            id: '1',
+            type: OrganizationType.NORMAL
+        },
+        organizations: [
+            {
+                name: 'Test',
+                id: '1',
+                type: OrganizationType.NORMAL
+            },
+            {
+                name: 'Test2',
+                id: '2',
+                type: OrganizationType.NORMAL
+            }
+        ]
     }
 }
 
